b46911a21c90a7b65189626e652f25054576eaa3
Juan Perez <jperez4432@gmail.com>
2021-07-09 23:37:46 -0500

    have api data displaying in the console log

---
+++ netflix-clone/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+import Row from "./Row";
+import requests from "./requests";
+function App() {
+  return (
+    <div className="App">
+    <h1>Beginning the netflix clone</h1>
+      <Row title="NETFLIX ORIGINALS" fetchUrl={requests.fetchNetflixOriginals} />
+      <Row title={"Trending Now"} fetchUrl={requests.fetchTrending} />
+    </div>
+  );
+}
+
+export default App;
